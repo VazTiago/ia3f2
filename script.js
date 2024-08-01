c562e00c4da81897970b5a2c3b505618b837a9d9
VazTiago <130671442+VazTiago@users.noreply.github.com>
2024-08-01 16:47:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const caixaprincipal = document.querySelector('caixa-principal');
+const caixaperguntas = document.querySelector('caixa-perguntas');
+const caixaalternativas = document.querySelector('caixa-alternativas');
+const caixaresultados = document.querySelector('caixa-resultado');
+
+const perguntas=[
+    enunciado: "pergunta 1",
+    alternativas:[
+        "alternativa 1",
+        "alternativa 2"
+    ]
+]
