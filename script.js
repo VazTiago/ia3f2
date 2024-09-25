ecb0b58dc3879324c5779a34e04e274e88cf0a4c
VazTiago <130671442+VazTiago@users.noreply.github.com>
2024-09-25 18:25:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,31 +3,32 @@ const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [ //serve para abrir lista de perguntas
+const perguntas = [   //serve para abrir lista de perguntas
     {//abre o objeto das perguntas
-        enunciado: "depay faz golhj ou nn",
-        alternativas: ["ss","nn"]
+        enunciado: "depay faz gol hj ou nn",
+        alternativas:[{texto:"ss",afirmação: "afirmação da alternativa 1"},{texto:"nn",afirmação: "afirmação da alternativa 2"}]
     },
     {
         enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 1","Alternativa 2"]
+
+        alternativas: [{texto:"Alternativa 1",afirmação: "afirmação da alternaiva 1"},{texto: "Alternativa 2", afirmação:"afirmação da alternativa 2"}]
     },
     {
         enunciado: "Pergunta 3",
-        alternativas: ["Alternativa 1","Alternativa 2"]
+        alternativas: [{texto:"Alternativa 1",afirmação: "afirmação da alternaiva 1"},{texto: "Alternativa 2", afirmação:"afirmação da alternativa 2"}]
     }
 ]
 
 let posicao = 0;
 let perguntaAtual;
 
-function mostraPergunta(){
+function mostraPergunta() {
     perguntaAtual = perguntas[posicao];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
